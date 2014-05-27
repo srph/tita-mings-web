c16a25e5ed7c224927faee822b49c892a4dfcdf2
srph <seraphipod@gmail.com>
2014-05-27 11:38:34 +0800

    Added link function for the product directive.

--- app/dir/product-index/module.js
+++ app/dir/product-index/module.js
@@ -1,11 +1,14 @@
-define(['../module'], function(app) {
+define(['../module', 'jquery'], function(app, $) {
 	'use strict';
 
 	return app.directive('productIndex', [function() {
 		return {
 
 			restrict: 'E',
-			templateUrl: 'app/dir/product-index/partial.html'
+			templateUrl: 'app/dir/product-index/partial.html',
+			link: function(scope, el) {
+				//
+			}
 
 		};
 	}]);
